14994bb659ebd5f100574a4dde74b9e5df1860ee
Jungma <mbjun3630@naver.com>
2023-06-21 19:59:00 +0900

    Initial commit

---
+++ src/test/java/io/bammyu/projectalpha/ProjectAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bammyu.projectalpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAlphaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
